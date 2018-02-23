12617d77c5d35948ba12f0643315a2e038b873b8
fanqi <me@fanqi.xyz>
2018-02-23 11:35:44 +0800

    change javascript to jquery style

--- src/main/resources/META-INF/resources/js/main.js
+++ src/main/resources/META-INF/resources/js/main.js
@@ -1,4 +1,3 @@
 $("#changeBackgroundColorBtn").click(function(){
-	var hello = document.getElementById("hello");
-	hello.style.backgroundColor="red";
+	$("#hello").css("background-color","red");
 });
